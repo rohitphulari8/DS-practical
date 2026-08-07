12aa21d48e8a29d9b95aa80a6ce280e6a49779c4
rohitphulari8 <rohitphulari8@gmail.com>
2026-08-07 15:43:42 +0530

    Add files via upload

---
+++ singly linked list.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+struct Node	
+{
+	int data ;
+	struct Node *Next ;
+};
+	
+int main()
+{
+	struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+	newNode->data = 10;
+	newNode->Next = NULL;
+	
+	cout<<newNode->data;
+	
+	return 0;
+}
